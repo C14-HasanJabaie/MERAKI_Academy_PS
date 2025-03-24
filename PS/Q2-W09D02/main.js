8375c59c543e82a1162ba34f60bdc7f6943febdc
HasanJabaie <hasan.m.jabaie@gmail.com>
2025-03-24 21:30:42 +0300

    q3

--- PS/Q2-W09D02/main.js
+++ PS/Q2-W09D02/main.js
@@ -11,30 +11,21 @@ Write a function that returns the sum of odd numbers in an array.
 */
 
 const sumOddNumber = (arr) => {
-
-  let counter = 0
+  let counter = 0;
   arr.forEach((ele) => {
-   
-    if(ele % 2){
-      counter = counter + ele
-
-
-  
+    if (ele % 2 != 0) {
+      counter = counter + ele;
     }
-    
   });
-  return counter
+
+  return counter;
 };
 // sumOddNumber([1, 2, 3, 4, 5]) // => 9
-sumOddNumber([1, 5, 3, 4, 5]) // => 14
-sumOddNumber([12, 2, 3, 4, 5, 0]) // => 8
-sumOddNumber([2, 4, 6,0 ]) // => 0
-sumOddNumber([1, 5, 3, 4, 5]) // => 14
-sumOddNumber([-2, 0, 4, -3, 5]) // => 2
-sumOddNumber([]) // => 0
-/*
-Examples:
-
-
-*/
-module.exports = { sumOddNumber };
+// sumOddNumber([1, 5, 3, 4, 5]) // => 14
+// sumOddNumber([12, 2, 3, 4, 5, 0]) // => 8
+// sumOddNumber([2, 4, 6,0 ]) // => 0
+// sumOddNumber([1, 5, 3, 4, 5]) // => 14
+// sumOddNumber([-2, 0, 4, -3, 5]) // => 2
+// sumOddNumber([]) // => 0
+// /*
+Examples: module.exports = { sumOddNumber };
--- PS/Q3-W09D02/main.js
+++ PS/Q3-W09D02/main.js
@@ -2,19 +2,37 @@
 
 /*  
 Write a function reverses the characters of every word of a sentence. sentences contain only English letter and space characters.
+1-i will change the string to array 
+2- i will jion array index
+3- i will change array to stirng 
 */
 
 const reverseEachWord = (str) => {
-  // YOUR CODE HERE
- 
-};
+  let arr= str.split("")
 
+  if(arr.length > 1){
+  
+let arr1 =arr.slice(0,5).reverse()
+let arr2 = arr.slice(6).reverse()
+let arr3 = arr1.concat(arr2)
+return arr3.toString(" ")
+    
+  }else{
+    return "Wrong!! it's empty string"
+  }
+
+  
+
+};
+// reverseEachWord("Hello world") // => "olleH dlrow"
+// reverseEachWord("Hello guys") // => "olleH syug"
+// reverseEachWord("It’s about what you can figure out") // => "s’tI tuoba tahw uoy nac erugif tuo"
+// reverseEachWord("      ") // => "Wrong!! it's empty string"
 /* 
 Examples:
-reverseEachWord("Hello world") // => "olleH dlrow"
-reverseEachWord("Hello guys") // => "olleH syug"
-reverseEachWord("It’s about what you can figure out") // => "s’tI tuoba tahw uoy nac erugif tuo"
-reverseEachWord("      ") // => "Wrong!! it's empty string"
+
+
+
 
 */
 module.exports = { reverseEachWord };
