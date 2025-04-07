8f0aa48a280e9964786630fa178de9a88e24221a
HasanJabaie <hasan.m.jabaie@gmail.com>
2025-04-07 19:49:08 +0300

    PS q8

--- PS/Q8-W10D02/main.js
+++ PS/Q8-W10D02/main.js
@@ -4,7 +4,13 @@
   For example, 2, 3, 5, 7, 11 are the first few prime numbers.*/
 
 const isPrime = (num) => {
-  // YOUR CODE HERE
+  if(num/num === 1 && num %2 === 0){
+    console.log(num)
+   
+  }else{
+    console.log("not A prime ")
+    
+  }
 };
-
+isPrime(4)
 module.exports = { isPrime };
