efa6b88f575697c4104942354e3e571cc1fc0dd0
HasanJabaie <hasan.m.jabaie@gmail.com>
2025-04-09 20:13:20 +0300

    ps11

--- PS/Q12-W10D04/main.js
+++ PS/Q12-W10D04/main.js
@@ -5,14 +5,21 @@ Find the first item which appear an even number of times in an array.
 */
 
 const evenAppearance = (array) => {
-  // YOUR CODE HERE
-};
+  count = 0
+  array.forEach(e=> {
+if(e%2 === 0){
+  count+=1
+  if(count ===2)
+  console.log(e)
 
-/*
-Examples:
+}   
+  });
+};
 evenAppearance([1, 1, 2, 6, 6]) // => 1
-evenAppearance([1, 2, 2, 9, 8, 8, 6, 6]) // => 2
+
+Examples:
+
+ evenAppearance([1, 2, 2, 9, 8, 8, 6, 6]) // => 2
 evenAppearance([1, 9, 7, 3, 6, 6, 8, 9, 9]) // => 6
 evenAppearance([1, 9, 7, 3, 6, 6, 8, 9]) // => 9
-*/
 module.exports = { evenAppearance };
