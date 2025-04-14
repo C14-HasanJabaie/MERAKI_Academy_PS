7f2643507ba8819b828a1349d0f6056f3ca6fb3a
HasanJabaie <hasan.m.jabaie@gmail.com>
2025-04-14 19:48:38 +0300

    ps14

--- PS/Q14-W11D02/main.js
+++ PS/Q14-W11D02/main.js
@@ -4,14 +4,22 @@
 Write a function calculates the Highest possible sum of any n consecutive numbers in an array arr of integers (where n ≥ 1).
 */
 
-const findHighestPossibleSum = (arr, num) => {
-  // YOUR CODE HERE
-};
+const array = [10, 2, 3, 4, 2, 6, 8]
+const arr1= array.sort()
+console.log(arr1)
+const sum = (arr1.length)+(arr1.length-1)
+console.log(sum)
 
+
+// const findHighestPossibleSum = (arr, num) => {
+   
+//   console.log(arr.sort())
+// };
+// findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8],2) // => 14
 /*
 Examples:
 findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 1) // => 10
-findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 2) // => 14
+
 findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 3) // => 16
 findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 4) // => 20
 findHighestPossibleSum([10, 2, 3, 4, 2, 6, 8], 5) // => 23
